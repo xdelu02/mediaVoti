63ab33a4ff3590fb15624c497cb72874efcddd56
AleFeri <50620354+AleFeri@users.noreply.github.com>
2021-01-31 19:53:05 +0100

    Creato il prompt per i dati (start.py)

--- start.py
+++ start.py
@@ -1,1 +1,13 @@
-#qua va un prompt che chiede codice della scuola, username e password
+from getpass import getpass
+
+print("--- Inserisci i dati di Argo ---")
+codiceScuola = input("Codice scuola: ")
+username = input("Username: ")
+password = getpass()
+
+# print()
+# 
+# print("--- I dati inseriti sono ---")
+# print("Codice scuola: " + codiceScuola + ", Username: " + username + ", Password: " + password)
+
+input()
