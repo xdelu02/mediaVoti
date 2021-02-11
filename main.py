5585d66678a85df1b079e091f8eec79fb72b2fc3
AleFeri <50620354+AleFeri@users.noreply.github.com>
2021-02-11 13:57:40 +0100

    aggiunta salvataggio dati

--- main.py
+++ main.py
@@ -4,30 +4,58 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import dataInputUI
+import data
 import re
 from datetime import datetime
 
 PATH = ".\chromedriver88.0.4324.96" # "E:\GoogleDriver\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
-codicescuola = dataInputUI.codScuola # sg18309
-username = dataInputUI.user
-password = dataInputUI.passw
+
+if not data.isEmpty(): 
+    while True:
+        c = input("Vuoi caricare i dati? [y o n]\n>")
+        if c == "y":
+            codicescuola = data.getCodice()
+            username = data.getUsername()
+            password = data.getPassword()
+            break
+        if c == "n":
+            import dataInputUI
+            codicescuola = dataInputUI.codScuola # sg18309
+            username = dataInputUI.user
+            password = dataInputUI.passw
+            break
+else:
+    import dataInputUI
+    codicescuola = dataInputUI.codScuola # sg18309
+    username = dataInputUI.user
+    password = dataInputUI.passw
 
 # prima pagina
 driver.get("http://"+codicescuola+".scuolanext.info")
 driver.maximize_window()
 
-try:
-    inputUsername = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.ID, "username"))
-    )
-    inputPassword = driver.find_element_by_id("password")
-    inputUsername.send_keys(username)
-    inputPassword.send_keys(password)
-    inputPassword.send_keys(Keys.RETURN)
-finally:
-    pass
+login = False
+while not login:
+    try:
+        inputUsername = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.ID, "username"))
+        )
+        inputPassword = driver.find_element_by_id("password")
+        inputUsername.send_keys(username)
+        inputPassword.send_keys(password)
+        inputPassword.send_keys(Keys.RETURN)
+    finally:
+        login = True
+
+if data.isEmpty():
+    while True:
+        c = input("Vuoi salvare i dati? [y o n]\n>")
+        if c == "y":
+            data.save(codicescuola, username, password)
+            break
+        if c == "n":
+            break
 
 # seconda pagina
 try:
@@ -42,7 +70,7 @@ mediaPrimoQuad = 0.0
 nVotiPrimoQuad = 0.0
 mediaSecondoQuad = 0.0
 nVotiSecondoQuad = 0.0
-dataFineQuadrimestre = datetime.strptime("31-01-2021", "%d-%m-%y") #poi ovviamente va modificato il 2021
+#dataFineQuadrimestre = datetime.strptime("31-01-2021", "%d-%m-%y") #poi ovviamente va modificato il 2021
 try:
     divVoti = WebDriverWait(driver, 20).until(
         EC.presence_of_element_located((By.ID, "sheet-sezioneDidargo:panel-votiGiornalieri:pannello"))
@@ -60,28 +88,30 @@ try:
             finally:
                 s = res.text
                 dataVotoHtml = driver.find_element_by_xpath('//*[@id="sheet-sezioneDidargo:panel-votiGiornalieri:form"]/fieldset[1]/table/tbody/tr/td[2]/b')
-                dataVoto = datetime.strptime(dataVotoHtml, "%d-%m-%y")
+                #dataVoto = datetime.strptime(dataVotoHtml, "%d-%m-%y")
                 s = re.sub("[a-zA-z\s()$&+,:;=?@#|'<>^*()%!-]", '', s) #rimuove tutti i caratteri dalla stringa a parte i numeri e i punti
                 if(s != None and s != ""): 
                     s = float(s)
-                    if(dataVoto <= dataFineQuadrimestre):
+                    if(True): # dataVoto <= dataFineQuadrimestre
                         mediaPrimoQuad += s
-                        mediaPrimoQuad += 1
+                        nVotiPrimoQuad += 1
                     else:
                         mediaSecondoQuad += s
                         nVotiSecondoQuad += 1
 
         mediaPrimoQuad /= nVotiPrimoQuad
-        mediaSecondoQuad /= nVotiSecondoQuad
+        #mediaSecondoQuad /= nVotiSecondoQuad
+
+        print("primo quadrimestre: " + str(mediaPrimoQuad))
+        #print("secondo quadrimestre" + mediaSecondoQuad)
         mediaPrimoQuad = 0.0
         nVotiPrimoQuad = 0.0
         mediaSecondoQuad = 0.0
         nVotiSecondoQuad = 0.0
-        print("primo quad" + mediaPrimoQuad)
-        print("secondo quad" + mediaSecondoQuad)
 finally:
     pass
 
 
 time.sleep(20)
+input()
 driver.quit()
